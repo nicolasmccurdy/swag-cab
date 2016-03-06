ef47eed2832a302f542f1552c7bd4f91c84e0ca3
Nicolas McCurdy <thenickperson@gmail.com>
2016-03-05 21:48:50 -0500

    Add validation to models

--- models.js
+++ models.js
@@ -3,19 +3,19 @@ mongoose.connect('mongodb://localhost/swagcab');
 
 module.exports = {
   Driver: mongoose.model('Driver', {
-    driverid: Number,
-    userid: Number,
-    seats: Number,
-    eventid: Number,
-    desc: String
+    driverid: { type: Number, required: true },
+    userid: { type: Number, required: true },
+    seats: { type: Number, required: true },
+    eventid: { type: Number, required: true },
+    desc: { type: String, required: true }
   }),
 
   Event: mongoose.model('Event', {
-    eventid: Number,
-    name: String,
-    location: String,
-    hostid: Number,
-    creationdate: Date,
-    queueid: Number
+    eventid: { type: Number, required: true },
+    name: { type: String, required: true, default: 'leparty' },
+    location: { type: String, required: true },
+    hostid: { type: Number, required: true },
+    creationdate: { type: Date, required: true },
+    queueid: { type: Number, required: true }
   })
 };
