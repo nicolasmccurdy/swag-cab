5fb6c620a59358c2c754097a7ea35ad5d84103f8
Nicolas McCurdy <thenickperson@gmail.com>
2016-03-05 21:58:18 -0500

    Remove extra ID fields

--- models.js
+++ models.js
@@ -3,7 +3,6 @@ mongoose.connect('mongodb://localhost/swagcab');
 
 module.exports = {
   Driver: mongoose.model('Driver', {
-    driverid: { type: Number, required: true },
     userid: { type: Number, required: true },
     seats: { type: Number, required: true },
     eventid: { type: Number, required: true },
@@ -11,7 +10,6 @@ module.exports = {
   }),
 
   Event: mongoose.model('Event', {
-    eventid: { type: Number, required: true },
     name: { type: String, required: true, default: 'leparty' },
     location: { type: String, required: true },
     hostid: { type: Number, required: true },
