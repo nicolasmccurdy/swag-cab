534f864ea63dfedbed62feb9715bd35bc409ad19
Nicolas McCurdy <thenickperson@gmail.com>
2016-03-05 21:07:04 -0500

    Start using Sequelize

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Sequelize = require('sequelize');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var sequelize = new Sequelize('database', null, null, { dialect: 'sqlite' });
+
+  var Driver = sequelize.define('Project', {
+    driverid: Sequelize.INTEGER,
+    userid: Sequelize.INTEGER,
+    seats: Sequelize.INTEGER,
+    eventid: Sequelize.INTEGER,
+    desc: Sequelize.STRING
+  });
+
+  var Event = sequelize.define('Task', {
+    eventid: Sequelize.INTEGER,
+    name: Sequelize.STRING,
+    location: Sequelize.STRING,
+    hostid: Sequelize.INTEGER,
+    creationdate: Sequelize.DATE,
+    queueid: Sequelize.INTEGER
+  });
 });
 
 module.exports = router;
